926ba48378abd16be6b7f86925ec4c0f3b304102
Tobshub <tobanigeorge@gmail.com>
2023-05-20 00:15:44 +0100

    added diesel migrations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,27 @@
-use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
+use diesel::{prelude::*, sqlite::Sqlite};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use dotenvy::dotenv;
 use std::env;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Failed to use database {}", database_url))
+    let mut connection = SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Failed to use database {}", database_url));
+
+    run_migrations(&mut connection);
+
+    return connection;
+}
+
+fn run_migrations(connection: &mut impl MigrationHarness<Sqlite>) {
+    connection
+        .run_pending_migrations(MIGRATIONS)
+        .expect("Failed to run db migrations");
 }
 
 pub mod models;
